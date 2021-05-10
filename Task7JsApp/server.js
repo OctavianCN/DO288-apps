8ec26214699bb3e8d25660708980feb7fb58ed45
OctavianCN <48841171+OctavianCN@users.noreply.github.com>
2021-05-10 14:14:41 +0300

    Update server.js

--- Task7JsApp/server.js
+++ Task7JsApp/server.js
@@ -7,7 +7,7 @@ const app = express()
 const port = 3000
 app.get('/', (req, res) =>{
 var con = mysql.createConnection({
- host: 172.30.108.147,
+ host: 172.17.0.13,
  user: user,
  password: pass,
  database: test
